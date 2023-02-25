24415d4eb6e29e71f3d3210903e50d25b8040ef7
beko123X <106892278+beko123X@users.noreply.github.com>
2023-02-25 17:14:26 +0200

    Update vice_doctor_anticoagulation_chart_controller.php

--- app/Http/Controllers/vice_doctor_anticoagulation_chart_controller.php
+++ app/Http/Controllers/vice_doctor_anticoagulation_chart_controller.php
@@ -7,12 +7,23 @@ use App\Models\vice_doctor_anticoagulation_chart;
 
 class vice_doctor_anticoagulation_chart_controller extends Controller
 {
+    /*
+    * Function Name : get_vice_doctor_anticoagulation_chart
+    * Function Job  : retrieve vice_doctor_anticoagulation_chart from api determined by $patient_id
+    * Parameters    : $patient_id
+    * Return        : routes all vice_doctor_anticoagulation_chart from the database determined by $patient_id
+    */
     public function get_vice_doctor_anticoagulation_chart($patient_id)
     {
         return vice_doctor_anticoagulation_chart::where('patient_id',$patient_id)->get();
     }
     
-    //
+    /*
+    * function name : post_vice_doctor_anticoagulation_chart
+    * function job  : send request to vice_doctor_anticoagulation_chart in api
+    * Parameters    : $req
+    * Return        : save vice_doctor_anticoagulation_chart in the database if the request true else is failed
+    */
     public function post_vice_doctor_anticoagulation_chart(Request $req)
     {
         $add = new vice_doctor_anticoagulation_chart;
