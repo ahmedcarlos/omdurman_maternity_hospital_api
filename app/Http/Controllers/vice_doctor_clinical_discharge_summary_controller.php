5ae5bb9f67176b971e359bcfeb8de21ca05081bd
beko123X <106892278+beko123X@users.noreply.github.com>
2023-02-25 17:26:54 +0200

    Update vice_doctor_clinical_discharge_summary_controller.php

--- app/Http/Controllers/vice_doctor_clinical_discharge_summary_controller.php
+++ app/Http/Controllers/vice_doctor_clinical_discharge_summary_controller.php
@@ -7,7 +7,12 @@ use App\Models\vice_doctor_clinical_discharge_summary;
 
 class vice_doctor_clinical_discharge_summary_controller extends Controller
 {
-
+    /*
+    * Function Name : get_vice_doctor_clinical_discharge_summary
+    * Function Job  : retrieve vice_doctor_clinical_discharge_summary from api determined by $patient_id
+    * Parameters    : $patient_id
+    * Return        : routes all vice_doctor_clinical_discharge_summary from the database determined by $patient_id
+    */
     public function get_vice_doctor_clinical_discharge_summary($patient_id)
     {
         return vice_doctor_clinical_discharge_summary::where('patient_id',$patient_id)->get();
