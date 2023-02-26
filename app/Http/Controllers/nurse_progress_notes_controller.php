3e6a80d3e53336fb57e159088f47a1bceda70c47
beko123X <106892278+beko123X@users.noreply.github.com>
2023-02-26 07:51:03 +0200

    Update nurse_progress_notes_controller.php

--- app/Http/Controllers/nurse_progress_notes_controller.php
+++ app/Http/Controllers/nurse_progress_notes_controller.php
@@ -7,12 +7,23 @@ use App\Models\nurse_progress_notes;
 
 class nurse_progress_notes_controller extends Controller
 {
+    /*
+    * Function Name : get_nurse_progress_notes
+    * Function Job  : retrieve nurse_progress_notes from api determined by $patient_id
+    * Parameters    : $patient_id
+    * Return        : routes all nurse_progress_notes from the database determined by $patient_id
+    */
     public function get_nurse_progress_notes($patient_id)
     {
         return nurse_progress_notes::where('patient_id',$patient_id)->get();
     }
     
-    //
+    /*
+    * function name : post_nurse_progress_notes
+    * function job  : send request to nurse_progress_notes in api
+    * Parameters    : $req
+    * Return        : save nurse_progress_notes in the database if the request true else is failed
+    */
     public function post_nurse_progress_notes(Request $req)
     {
         $add = new nurse_progress_notes;
